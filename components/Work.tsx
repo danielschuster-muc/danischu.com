683917bf39bd957d7d06d01a6149d3de5c99aa09
Daniel Schuster <daniel.a.schuster@t-online.de>
2023-03-03 23:24:41 +0100

    update texts again

--- components/Work.tsx
+++ components/Work.tsx
@@ -14,7 +14,7 @@ const projects: ProjectType[] = [
     summary:
       "An API with data from the Harry Potter Universe: Characters, Movies, Books, Spells and Potions.",
     description:
-      "As a huge Harry Potter fan I create Potter DB using Next.js for the website and Ruby on Rails + PostgreSQL for the API. The API itself provides a vast amount of data related to the Harry Potter Universe such as information on characters, movies, books, spells, and potions, making it a valuable resource for developers and fans of the series alike. Users can search for data using the DB Search function or access it via the API (REST or GraphQL).",
+      "As a huge Harry Potter fan I always wanted to create a Harry Potter API, so I made one. It is built with Ruby on Rails + PostgreSQL for the API and Next.js for the website where users can search for data.The API itself provides a vast amount of data related to the Harry Potter Universe such as information on characters, movies, books, spells, and potions, making it a valuable resource for developers and fans of the series alike (such a me). Users can search for data using the DB Search function or access it via the API (REST or GraphQL).",
     images: [pd1Img.src],
     technologies: ["react", "next.js", "rails", "mui"],
     source: "https://github.com/danielschuster-muc/potter-db",
@@ -25,7 +25,7 @@ const projects: ProjectType[] = [
     summary:
       "If Stack Overflown was a website, it would be full of programming errors and typos.",
     description:
-      "I created Stack Overflown as a humorous take on Stack Overflow. Made with Vue.js it surprises visitors who accidentally misspell the URL, by giving them random jokes and puns instead of the usual technical help.",
+      "I created Stack Overflown as a humorous take on Stack Overflow. Made with Vue.js, it surprises visitors who accidentally misspell the URL, by giving them random jokes and puns instead of the usual technical help.",
     images: [soImg.src],
     technologies: ["vue"],
     source: "https://github.com/danielschuster-muc/stackoverflown",
@@ -36,7 +36,7 @@ const projects: ProjectType[] = [
     summary:
       "A portfolio page about me, my work and my preferred technologies.",
     description:
-      "My portfolio page - a personal website that showcases my work and projects as well as my preferred technologies. I built it using React + Next.js and for the visual appealing I used Tailwind CSS.",
+      "My portfolio is a personal website that showcases my work and projects as well as tools and technologies I frequently use when coding. I built it with React + Next.js, and for the visual appealing I used Tailwind CSS.",
     images: [portfolioImg.src],
     technologies: ["react", "next.js", "tailwind"],
     source: "https://github.com/danielschuster-muc/portfolio",
