0719dd0ab9245a72b488d196feb7157863b4813c
Daniel Schuster <daniel.a.schuster@t-online.de>
2023-04-16 01:47:17 +0200

    update about section

--- components/About.tsx
+++ components/About.tsx
@@ -26,7 +26,8 @@ const calcAge = (date: Date) => {
   return Math.abs(ageDate.getUTCFullYear() - 1970);
 };
 
-const age = calcAge(new Date(2003, 3, 23));
+// don't ask me why I'm using a date object here instead of a string, i just did it
+const age = calcAge(new Date(2003, 3, 23)); // why 23rd of April? Because it's my birthday! 🎉
 
 const About = () => {
   return (
@@ -59,20 +60,24 @@ const About = () => {
               About.
             </h2>
             <p className="mb-3 font-normal text-gray-500 dark:text-gray-300">
-              I am currently studying Computer Science with a minor in Business Administration at
-              Ludwig-Maximilians-Universität in Munich. <br />
-              I have been passionate about technology and programming for a long time. I have always
-              loved creating things and started my &quot;programming career&quot; with making small
-              websites, Minecraft modifications and Discord bots. <br />
+              I have been studying Computer Science with a minor in Business Administration at
+              Ludwig-Maximilians-Universität in Munich since October 2022. In addition, I am also
+              active member of the student initiative Enactus München e.V. in the Techdalo project,
+              which helps Colombian school graduates finding a job in IT. <br />
+              Already during school I discovered my interest in programming, created my first small
+              websites, developed Minecraft modifications and programmed Discord bots.
+              <br />
               After graduating from high school in July 2021, I participated in a web development
-              bootcamp at Le Wagon Munich. It was a very intensive and interesting time where I
-              learned a lot about web development. Afterwards I did a 6-week software internship at
-              QAware GmbH, where I could peek into the work of professional programming. <br />
-              During the gap between school and start of University I also went to a language course
-              to Valencia, Spain, and worked for a startup, where I was responsible for developing
-              the database and website. <br />
-              Overall it has been an exciting journey so far for me and I am excited to continue my
-              studies and see where my career takes me.
+              bootcamp at Le Wagon Munich. It was a very intensive and exciting time, in which I
+              dived deep into web development. Afterwards I did a 6-week software internship at
+              QAware GmbH, where I could peek into the work of professional programming during a
+              project for a large german car manufacturer. My tasks included the creation of web
+              components to display check control messages and the setup of automated UI tests.
+              <br />
+              Afterwards, I worked as a freelancer for 2 months to support the development of a
+              start-up in the field of &quot;workation as a service&quot; and mainly took care of
+              database setup and data model creation & validation. Until the start of my studies I
+              was helping out at the Münchner Tafel.
             </p>
             <ol className="items-center p-2 sm:flex">
               {events.map((e) => (
